273c17a6aa217a6041c775b0bcd6ff510d2fe8f6
James <jamesduv0012@gmail.com>
2023-08-13 14:03:49 -0600

    read me updated

--- copy_case_setup.sh
+++ copy_case_setup.sh
@@ -6,13 +6,20 @@
 #   $1: the slant angle
 #===============================================================================
 angle=$1
-system_path="${AHMED_REPO}/case_setup/system"
-constant_path="${AHMED_REPO}/case_setup/constant"
-init_path="${AHMED_REPO}/case_setup/0"
-slurm_path="${AHMED_REPO}/case_setup/slurm"
+system_path="${AHMED_REPO_PUB}/case_setup/system"
+constant_path="${AHMED_REPO_PUB}/case_setup/constant"
+init_path="${AHMED_REPO_PUB}/case_setup/0"
+slurm_path="${AHMED_REPO_PUB}/case_setup/slurm"
 case_path="${AHMED_SLANT_PATH}/slant_angle_${angle}"
 
 cp -r $system_path $case_path
 cp -r $constant_path $case_path
 cp -r $init_path $case_path
 cp -r $slurm_path $case_path
+
+# replace ANGLE with ${angle} in slurm files
+case_slurm_path="${case_path}/slurm/"
+rep_str="ANGLE"
+for file in case_slurm_path; do
+    sed -i -e "s/${rep_str}/${angle}/g" $file;
+done
